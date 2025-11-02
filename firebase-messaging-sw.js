343c2540c38126528158c2f39141caf2bfacc4b8
Agam Agarwal <agammaster@gmail.com>
2025-11-02 13:37:32 +0530

    Set notification image

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,21 +2,25 @@
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
 // Replace 10.13.2 with latest version of the Firebase JS SDK.
-importScripts('https://www.gstatic.com/firebasejs/12.5.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.5.0/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.5.0/firebase-app-compat.js",
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.5.0/firebase-messaging-compat.js",
+);
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -25,15 +29,19 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
+    "[firebase-messaging-sw.js] Received background message ",
+    payload,
   );
   // Customize notification here
   const notificationTitle = payload["notification"]["title"];
   const notificationOptions = {
     body: payload["notification"]["body"],
-    icon: '/img/logo.png'
+    icon: "/img/logo.png",
   };
+
+  if (payload["notification"]["image"]) {
+    notificationOptions.image = payload["notification"]["image"];
+  }
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
