6ec4ff757bd0753c5b329e2937674feb74c800a7
Agam Agarwal <agammaster@gmail.com>
2026-01-18 12:28:43 +0530

    Update content with bride and groom's names and hashtag wherever needed.
    Firebase and instagram to be updated later, once set up.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -36,7 +36,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload["notification"]["title"];
   const notificationOptions = {
     body: payload["notification"]["body"],
-    icon: "https://dilhariya.github.io/img/logo.png",
+    icon: "https://shahiway.github.io/img/logo.png",
   };
 
   if (payload["notification"]["image"]) {
