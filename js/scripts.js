07f34848aa11162e8acac15d0b162efba7125324
Agam Agarwal <agammaster@gmail.com>
2026-01-25 00:13:36 +0530

    Delete unused alert_markup function

--- js/scripts.js
+++ js/scripts.js
@@ -150,10 +150,3 @@ $(document).ready(function () {
         $('#btn-show-content').toggleClass('toggle-map-content');
     });
 });
-
-/********************** Extras **********************/
-
-// alert_markup
-function alert_markup(alert_type, msg) {
-    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
-}
